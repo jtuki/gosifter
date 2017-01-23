6758b7675d5e7044b27120e036a956b09f69f8a9
jiangtao <jiangtao@ablecloud.cn>
2017-01-23 17:14:09 +0800

    add test for confidential level sifter

--- api/sifter_test.go
+++ api/sifter_test.go
@@ -45,6 +45,33 @@ func marshalThenUnmarshal(t *testing.T, name string, s interface{}, us1, us2 int
 	}
 }
 
+// 带有筛选等级的序列化操作
+func leveldMarshal(t *testing.T, name string, s interface{}, maxConfidentialLevel int) {
+	fmt.Printf("=== %s ===\n", name)
+
+	// 原始的 json marshal 结果
+	var jsonBytes, siftBytes []byte
+	var siftedMap map[string]interface{}
+	var err error
+	if jsonBytes, err = json.Marshal(s); err != nil {
+		t.Fatal(err)
+	} else {
+		fmt.Printf("original json s1: %s\n", jsonBytes)
+	}
+
+	// 筛过之后的 json marshal 结果
+	if siftedMap, err = SiftStruct(s, maxConfidentialLevel); err != nil {
+		t.Fatal(err)
+	} else {
+		fmt.Println("siftedS1:", siftedMap)
+		if siftBytes, err = json.Marshal(siftedMap); err != nil {
+			t.Fatal(err)
+		} else {
+			fmt.Printf("siftedS1 json: %s\n", siftBytes)
+		}
+	}
+}
+
 func TestSiftStruct(t *testing.T) {
 	type T uint64
 	type S1 struct {
@@ -83,10 +110,12 @@ func TestSiftStruct(t *testing.T) {
 		}
 	}()
 
-	//
+	// --------------------------------------------------------------------
 
 	type S2 struct {
 		S1 `json:"s1_struct"`
+		// S1
+		// Struct1_Name S1
 
 		S21 string  `json:"s_21"`
 		S22 int32   `json:"s_22,omitempty"`
@@ -102,6 +131,7 @@ func TestSiftStruct(t *testing.T) {
 		S24: T(1666),
 		S25: float64(2666.666),
 	}
+	// s2.Struct1_Name = s1
 	s2.S1 = s1
 
 	func() {
@@ -113,5 +143,43 @@ func TestSiftStruct(t *testing.T) {
 			fmt.Printf("unmarshal: us1[%+v]\n", us1)
 			fmt.Printf("unmarshal: us2[%+v]\n", us2)
 		}
+	}()
+
+	// --------------------------------------------------------------------
+
+	type S3 struct {
+		S1 `confidential:"level2"`
+		// S1 `json:"s1_struct" confidential:"level2"`
+
+		S31 string  `json:"s_31" confidential:"level3"`
+		S32 int32   `json:"s_32,omitempty"`
+		S33 float64 `json:"-"`
+		S34 T       `json:",omitempty"`
+		S35 float64 `json:"s_35"`
+	}
+
+	s3 := S3{
+		S31: "S31_value",
+		S32: int32(1666),
+		S33: float64(1666.666),
+		S34: T(1666),
+		S35: float64(2666.666),
+	}
+	s3.S1 = s1
+
+	func() {
+		var us1, us2 S3
+		marshalThenUnmarshal(t, "s3", s3, &us1, &us2)
+		if us1 != us2 {
+			t.Fatalf("unmarshal: not the same")
+		} else {
+			fmt.Printf("unmarshal: us1[%+v]\n", us1)
+			fmt.Printf("unmarshal: us2[%+v]\n", us2)
+		}
+
+		leveldMarshal(t, "s3-level0", s3, CONFIDENTIAL_LEVEL0)
+		leveldMarshal(t, "s3-level1", s3, CONFIDENTIAL_LEVEL1)
+		leveldMarshal(t, "s3-level2", s3, CONFIDENTIAL_LEVEL2)
+		leveldMarshal(t, "s3-level3", s3, CONFIDENTIAL_LEVEL3)
 	}()
 }
